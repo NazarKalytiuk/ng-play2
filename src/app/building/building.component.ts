b21217ba5c0551703cbe3da9b620035caf02618d
Andrei Panov <andrei.panov@sap.com>
2020-03-25 20:22:04 +0100

    add building service

--- src/app/building/building.component.ts
+++ src/app/building/building.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuildingComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log(`new buildComponent created ${JSON.stringify(this)}`)
+  }
 
   ngOnInit(): void {
   }
